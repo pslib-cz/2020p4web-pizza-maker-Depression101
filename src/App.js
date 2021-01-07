5d6ca3efff073b1409e49e1692b7112b5a3ff170
Daniel Mertl <u17.417@pslib.cloud>
2021-01-07 15:36:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Navbar, NavbarBrand, NavbarToggler } from "reactstrap";
+import { render } from '@testing-library/react';
+var bool = false;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar expand="md">
+          <NavbarBrand href="/order/pizza">Pizza</NavbarBrand>
+          <NavbarToggler onClick={toggle()}></NavbarToggler>
+          
+        </Navbar>
+          <Switch>
+            <Route path="/order/pizza">
+              
+            </Route>
+            <Route path="/order/calzone">
+              
+            </Route>
+            <Route path="/">
+              
+            </Route>
+          </Switch>
+      </Router>
     </div>
   );
 }
-
+function toggle(){
+  bool= !bool;
+  render();
+};
+function Bool(){
+  if(bool)
+  {
+    return <h2>Bool is true</h2>
+  }
+  else
+    return <h2>Bool is false</h2>
+};
 export default App;
